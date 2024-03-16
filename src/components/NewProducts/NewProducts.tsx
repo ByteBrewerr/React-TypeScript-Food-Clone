8a87d56d34e90436baa2b7fdd623cb561f2cd2f6
ByteBrewerr <yarr.sova@mail.ru>
2024-03-16 23:22:59 +0300

    build v4

--- src/components/NewProducts/NewProducts.tsx
+++ src/components/NewProducts/NewProducts.tsx
@@ -1,34 +1,24 @@
 import { FC } from "react";
-import ReactDOM from "react-dom";
 import { usePopUp } from "../../hooks/usePopUp";
 import NewProductsPopup from "../../shared/modals/NewProducts/NewProductsPopup";
 import newProducts from "../../assets/newProducts.jpg";
-import "./NewProducts.scss";
 import Overlay from "../../shared/modals/Overlay/Overlay";
+import "./NewProducts.scss";
+import { Portal } from "@mui/material";
 
 const NewProducts: FC = () => {
   const { isPopUpVisible, handlePopUp } = usePopUp();
-
-  const portalContainer = document.getElementById("portal-container");
-
-  if (!portalContainer) {
-    alert("no portal");
-    return null;
-  }
 
   return (
     <div>
       <img className="NewProductsImage" src={newProducts} alt="New Products" onClick={handlePopUp} />
 
       {isPopUpVisible && (
-        <>
-          {ReactDOM.createPortal(
-            <Overlay handlePopup={handlePopUp}>
-              <NewProductsPopup onClose={handlePopUp} />
-            </Overlay>,
-            portalContainer
-          )}
-        </>
+        <Portal>
+          <Overlay handlePopup={handlePopUp}>
+            <NewProductsPopup onClose={handlePopUp} />
+          </Overlay>
+        </Portal>
       )}
     </div>
   );
