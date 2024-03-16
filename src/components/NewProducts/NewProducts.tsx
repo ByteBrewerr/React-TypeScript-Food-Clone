dbd0427c9baeb0e1d105277881fa872ff2b9f29c
ByteBrewerr <yarr.sova@mail.ru>
2024-03-16 23:54:07 +0300

    build v6

--- src/components/NewProducts/NewProducts.tsx
+++ src/components/NewProducts/NewProducts.tsx
@@ -1,10 +1,10 @@
 import { FC } from "react";
 import { usePopUp } from "../../hooks/usePopUp";
-import NewProductsPopup from "../../shared/modals/NewProducts/NewProductsPopup";
 import newProducts from "../../assets/newProducts.jpg";
 import Overlay from "../../shared/modals/Overlay/Overlay";
 import "./NewProducts.scss";
 import Portal from "../../shared/Portal/Portal";
+import NewProductsPopup from "../../../src/shared/modals/newProducts/newProductsPopup";
 
 const NewProducts: FC = () => {
   const { isPopUpVisible, handlePopUp } = usePopUp();
